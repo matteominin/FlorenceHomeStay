fd347fa01dad5e2394c4e41d013482bce32389b1
Matteo Minin <matteominin@yahoo.it>
2025-06-13 14:57:13 +0200

    Not found page

--- frontend/src/pages/notFound/NotFound.jsx
+++ frontend/src/pages/notFound/NotFound.jsx
@@ -1,13 +1,20 @@
 import Header from '../../components/header/Header';
+import Button from '../../components/button/Button';
 import './NotFound.scss';
 
 const NotFound = () => {
+
     return (
         <div className='not-found'>
-            <Header />
-            NotFound
+            <div className="not-found__content">
+                <h1>404 - Page Not Found</h1>
+                <p>The page you are looking for does not exist.</p>
+                <Button onClick={() => { window.location.href = '/' }} variant="primary">
+                    Return to Home
+                </Button>
+            </div>
         </div>
-    )
+    );
 }
 
-export default NotFound
+export default NotFound;
