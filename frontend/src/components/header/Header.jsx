cadffa1ea320db0e20ee65641c728868dd36389c
Matteo Minin <matteominin@yahoo.it>
2025-06-13 11:11:39 +0200

    Responsive header

--- frontend/src/components/header/Header.jsx
+++ frontend/src/components/header/Header.jsx
@@ -1,8 +1,15 @@
+import { useState } from 'react'
 import Button from '../button/Button'
 import logo from '../../assets/logo-black.png'
 import './Header.scss'
 
 function Header() {
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setIsMenuOpen(!isMenuOpen);
+    }
+
     return (
         <header className='scheme-1'>
             <div className="left">
@@ -10,7 +17,7 @@ function Header() {
                     <img src={logo} alt="logo Florence HomeStay" />
                 </a>
 
-                <ul>
+                <ul className='desktop-nav'>
                     <li>
                         <a href="/apartment">Apartment</a>
                     </li>
@@ -26,8 +33,36 @@ function Header() {
                 </ul>
             </div>
 
-            <Button>
-                <a href="">Book Your Stay!</a>
+            {/* Burger menu icon */}
+            <div className="burger-menu" onClick={toggleMenu}>
+                <div className={`line ${isMenuOpen ? 'open' : ''}`}></div>
+                <div className={`line ${isMenuOpen ? 'open' : ''}`}></div>
+                <div className={`line ${isMenuOpen ? 'open' : ''}`}></div>
+            </div>
+
+            {/* Mobile navigation */}
+            <ul className={`mobile-nav ${isMenuOpen ? 'open' : ''}`}>
+                <li>
+                    <a href="/apartment" onClick={toggleMenu}>Apartment</a>
+                </li>
+                <li>
+                    <a href="/travel-tips" onClick={toggleMenu}>Travel Tips</a>
+                </li>
+                <li>
+                    <a href="/about-us" onClick={toggleMenu}>About Us</a>
+                </li>
+                <li>
+                    <a href="/contact-us" onClick={toggleMenu}>Contact Us</a>
+                </li>
+                <li>
+                    <Button className="cta mobile-cta" onClick={toggleMenu}>
+                        <a href="#">Book Your Stay!</a>
+                    </Button>
+                </li>
+            </ul>
+
+            <Button className="desktop-cta">
+                <a href="#">Book Your Stay!</a>
             </Button>
         </header>
     )
